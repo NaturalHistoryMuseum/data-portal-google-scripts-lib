1e8010648c66c484893765bf2411951baee7f472
Josh Humphries <j.humphries@nhm.ac.uk>
2021-11-04 09:37:36 +0000

    Add invertebrate sheet code

---
+++ src/NHM_DToL_Invertebrate (non-insect)_Species/Code.js
@@ -0,0 +1,14 @@
+function reset() {
+    DToLLib.resetMark();
+}
+
+function main() {
+    DToLLib.updateCounts({
+        targetSheet: 'progress',
+        limit: 300,
+        genusColumn: 'H',
+        speciesColumn: 'I',
+        ethanolColumn: 'L',
+        frozenColumn: 'O',
+    })
+}
