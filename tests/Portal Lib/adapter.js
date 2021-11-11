19c4ecae5f93f853c5350a2ec9cb37a2dd770028
Josh Humphries <j.humphries@nhm.ac.uk>
2021-11-11 13:43:47 +0000

    Add explanation to test adapter file

--- tests/Portal Lib/adapter.js
+++ tests/Portal Lib/adapter.js
@@ -1,3 +1,6 @@
+// this adapter creates a module out of the Portal Lib. We can't include a module.exports line in the actual lib
+// because it makes Google cry so we have to do it here just for the tests. We could do this in the test files but doing
+// it like this keeps the scopes clean and allows reuse across multiple test files.
 const fs = require('fs');
 eval(fs.readFileSync(__dirname + '/../../src/Portal Lib/lib.js') + '');
 module.exports = load();
