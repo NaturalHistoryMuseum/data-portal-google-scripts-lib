9effcf900efacb0f06837bef798759162cc9bbab
Josh Humphries <j.humphries@nhm.ac.uk>
2021-12-02 00:07:50 +0000

    Fix tests

--- tests/adapter.js
+++ tests/adapter.js
@@ -2,5 +2,5 @@
 // because it makes Google cry so we have to do it here just for the tests. We could do this in the test files but doing
 // it like this keeps the scopes clean and allows reuse across multiple test files.
 const fs = require('fs');
-eval(fs.readFileSync(__dirname + '/../../src/Portal Lib/lib.js') + '');
+eval(fs.readFileSync(__dirname + '/../src/lib.js') + '');
 module.exports = load();
